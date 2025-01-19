b38361ccf4e509eb9396d7ec69e3e485621a245e
sidhys <sidhys1@gmail.com>
2025-01-19 01:41:56 +0000

    add commoncrypto support for hashing

--- native/src/hash.cpp
+++ native/src/hash.cpp
@@ -1,5 +1,10 @@
 #include "bouncer/hash.hpp"
 
+#if defined(__APPLE__) && __has_include(<CommonCrypto/CommonDigest.h>)
+#include <CommonCrypto/CommonDigest.h>
+#define BOUNCER_HAS_COMMONCRYPTO 1
+#endif
+
 #include <iomanip>
 #include <sstream>
 
@@ -7,6 +12,23 @@ namespace bouncer {
 namespace {
 
 std::string stable_hash_hex_bytes(const std::uint8_t *data, std::size_t size) {
+#if defined(BOUNCER_HAS_COMMONCRYPTO)
+  auto hex_bytes = [](const unsigned char *bytes, std::size_t count) {
+    static constexpr char digits[] = "0123456789abcdef";
+    std::string out;
+    out.reserve(count * 2);
+    for (std::size_t i = 0; i < count; ++i) {
+      const unsigned char byte = bytes[i];
+      out.push_back(digits[byte >> 4]);
+      out.push_back(digits[byte & 0x0f]);
+    }
+    return out;
+  };
+
+  unsigned char digest[CC_SHA256_DIGEST_LENGTH];
+  CC_SHA256(data, static_cast<CC_LONG>(size), digest);
+  return hex_bytes(digest, CC_SHA256_DIGEST_LENGTH);
+#else
   constexpr std::uint64_t fnv_offset = 14695981039346656037ull;
   constexpr std::uint64_t fnv_prime = 1099511628211ull;
 
@@ -28,6 +50,7 @@ std::string stable_hash_hex_bytes(const std::uint8_t *data, std::size_t size) {
   const auto left = hash_for(0x243f6a8885a308d3ull);
   const auto right = hash_for(0x9e3779b97f4a7c15ull);
   return hex64(left) + hex64(right);
+#endif
 }
 
 } // namespace
