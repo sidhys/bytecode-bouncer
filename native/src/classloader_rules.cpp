1818a72fa4d92bd7c3950106dd1e446237634a98
sidhys <sidhys1@gmail.com>
2025-01-27 01:59:43 +0000

    add class loader chain processing

--- native/src/classloader_rules.cpp
+++ native/src/classloader_rules.cpp
@@ -12,6 +12,25 @@ bool starts_with(const std::string &value, const std::string &prefix) {
          value.compare(0, prefix.size(), prefix) == 0;
 }
 
+std::string chain_detail(const ClassLoaderEvent &event) {
+  std::ostringstream detail;
+  detail << "loader="
+         << (event.loader_name.empty() ? "<bootstrap>" : event.loader_name);
+  if (!event.code_source.empty()) {
+    detail << " source=" << event.code_source;
+  }
+  if (!event.parent_chain.empty()) {
+    detail << " parents=";
+    for (std::size_t i = 0; i < event.parent_chain.size(); ++i) {
+      if (i > 0) {
+        detail << " -> ";
+      }
+      detail << event.parent_chain[i];
+    }
+  }
+  return detail.str();
+}
+
 } // namespace
 
 ClassLoaderRuleModel::ClassLoaderRuleModel() {
@@ -20,6 +39,8 @@ ClassLoaderRuleModel::ClassLoaderRuleModel() {
   add_allowed_loader("bootstrap");
   add_allowed_loader("platform");
   add_allowed_loader("app");
+  add_allowed_loader("jdk.internal.loader.ClassLoaders$PlatformClassLoader");
+  add_allowed_loader("jdk.internal.loader.ClassLoaders$AppClassLoader");
 }
 
 void ClassLoaderRuleModel::add_allowed_loader(std::string loader_name) {
