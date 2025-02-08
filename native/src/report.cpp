75aa54128ea0c742dcb96fd613f57cb4f6a48205
sidhys <sidhys1@gmail.com>
2025-02-08 00:00:43 +0000

    add json_escape function

--- native/src/report.cpp
+++ native/src/report.cpp
@@ -1,11 +1,46 @@
 #include "bouncer/report.hpp"
 
+#include <iomanip>
+#include <sstream>
+
 namespace bouncer {
 
 ReportEvent report_from_detection(const Detection &detection,
                                   std::map<std::string, std::string> fields) {
   return ReportEvent{detection.severity, detection.kind, detection.subject,
                      detection.detail, std::move(fields)};
+}
+
+std::string json_escape(const std::string &value) {
+  std::ostringstream out;
+  for (const char c : value) {
+    switch (c) {
+    case '\\':
+      out << "\\\\";
+      break;
+    case '"':
+      out << "\\\"";
+      break;
+    case '\n':
+      out << "\\n";
+      break;
+    case '\r':
+      out << "\\r";
+      break;
+    case '\t':
+      out << "\\t";
+      break;
+    default:
+      if (static_cast<unsigned char>(c) <= 0x20) {
+        out << "\\u" << std::hex << std::setw(4) << std::setfill('0')
+            << static_cast<int>(static_cast<unsigned char>(c));
+      } else {
+        out << c;
+      }
+      break;
+    }
+  }
+  return out.str();
 }
 
 } // namespace bouncer
